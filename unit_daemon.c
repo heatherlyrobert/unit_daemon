a5dc46a53482109708c30a029defca91ada7a0ee
heatherlyrobert <heatherlyrobert@gmail.com>
2022-11-29 22:20:04 -0800

    1.5a : created a quiet heartbeat version for signals

---
+++ unit_daemon.c
@@ -0,0 +1,198 @@
+#include "unit_daemon.h"
+
+
+typedef struct tm        tTIME;
+static char    x_term    = '-';
+static char    x_cpu     = '-';
+
+
+
+void             /* [------] receive signals ---------------------------------*/
+incomming          (int a_signal, siginfo_t *a_info, char *a_name, char *a_desc)
+{
+   /*---(locals)-----------+-----+-----+-*/
+   FILE       *f           = NULL;
+   int         x_pid       = 0;
+   char        t           [LEN_DESC]  = "";
+   char        x_recd      [LEN_HUND]  = "";
+   /*---(message)-------------------------------*/
+   x_pid     = getpid ();
+   f = fopen ("/tmp/signal_local.log", "at");
+   sprintf (t, "local  %-7.7s (%2d) %s", a_name, a_signal, a_desc);
+   yEXEC_heartquiet (x_pid, 0, t, NULL, x_recd);
+   if (f != NULL) {
+      fprintf (f, "%s\n", x_recd);
+      fflush (f);
+      fclose (f);
+      f = NULL;
+   }
+   /*---(check signal)-------------------*/
+   switch (a_signal) {
+   case  SIGCHLD:
+      break;
+   case  SIGHUP:
+      break;
+   case  SIGUSR1:
+      break;
+   case  SIGUSR2:
+      break;
+   case  SIGALRM:
+      if (x_cpu  != '-')  _exit (0);
+      break;
+   case  SIGSEGV:
+      break;
+   case  SIGTERM:
+      sleep (1);
+      if (x_term == 'y')  _exit (SIGTERM);
+      break;
+   case  SIGABRT:
+      break;
+   case  SIGTSTP:
+      break;
+   case  SIGCONT:
+      break;
+   case  SIGINT :
+      break;
+   case  SIGQUIT:
+      break;
+   default      :
+      break;
+   }
+   /*---(complete)-----------------------*/
+   return;
+}
+
+int
+main               (int a_argc, char *a_argv [])
+{
+   /*---(locals)-----------+-----+-----+-*/
+   char       *p           =  0x2;
+   char        x_daemon    =  'y';
+   int         x_loop      =    2;
+   char        rc          =    0;
+   int         i           =    0;
+   char        x_bullet    = YEXEC_SOFT;
+   char        x_inter     = YEXEC_NO;
+   char        x_child     = YEXEC_WAIT;
+   FILE       *f           = NULL;
+   int         x_pid       =   -1;
+   int         x_ppid      =   -1;
+   int         x_uid       =   -1;
+   char        x_root      =  '-';
+   char        x_who       [LEN_LABEL] = "";
+   char        x_proj      [LEN_LABEL] = "";
+   char        x_home      [LEN_HUND]  = "";
+   int         x_hup       =    0;
+   int         a           =    0;
+   int         b           =    0;
+   char        x_mem       =  '-';
+   /*---(set up signal handling)---------*/
+   for (i = 1; i < a_argc; ++i) {
+      if (strcmp (a_argv [i], "--bulletproof"   ) == 0)   x_bullet = YEXEC_HARD;
+      if (strcmp (a_argv [i], "--interactive"   ) == 0)   x_inter  = YEXEC_YES;
+      if (strcmp (a_argv [i], "--children"      ) == 0)   x_child  = YEXEC_YES;
+   }
+   /*---(determine looping)--------------*/
+   for (i = 1; i < a_argc; ++i) {
+      if (strcmp (a_argv [i], "--nodaemon") == 0)    x_daemon = '-';
+      if (strcmp (a_argv [i], "--graceful") == 0)    x_term   = 'y';
+      if (strcmp (a_argv [i], "--infinite") == 0)    x_loop = 99999;
+      if (strcmp (a_argv [i], "--super"   ) == 0)    x_loop = 60;
+      if (strcmp (a_argv [i], "--longer"  ) == 0)    x_loop = 20;
+      if (strcmp (a_argv [i], "--long"    ) == 0)    x_loop = 10;
+      if (strcmp (a_argv [i], "--4s"      ) == 0)    x_loop = 4;
+      if (strcmp (a_argv [i], "--short"   ) == 0)    x_loop = 1;
+      if (strcmp (a_argv [i], "--instant" ) == 0)    x_loop = 0;
+      if (strcmp (a_argv [i], "--hup"     ) == 0)    x_hup  = atoi (a_argv [++i]);
+      if (strcmp (a_argv [i], "--Lcpu"    ) == 0)    x_cpu  = 'L';
+      if (strcmp (a_argv [i], "--Mcpu"    ) == 0)    x_cpu  = 'M';
+      if (strcmp (a_argv [i], "--Hcpu"    ) == 0)    x_cpu  = 'H';
+      if (strcmp (a_argv [i], "--normal"  ) == 0)    x_loop = 2;
+      if (strcmp (a_argv [i], "--Lmem"    ) == 0)    x_mem  = 'L';
+      if (strcmp (a_argv [i], "--Mmem"    ) == 0)    x_mem  = 'M';
+      if (strcmp (a_argv [i], "--Hmem"    ) == 0)    x_mem  = 'H';
+      /*---(complicated)-----------------*/
+      if (strcmp (a_argv [i], "--segv"    ) == 0) {
+         printf ("%s", p);
+         x_loop = 2;
+      }
+      if (strcmp (a_argv [i], "--badrc"   ) == 0) {
+         x_daemon = '-';
+         x_loop = 1;
+         rc     = -12;
+      }
+      if (strcmp (a_argv [i], "--whoami"  ) == 0) {
+         rc = yEXEC_whoami (&x_pid, &x_ppid, &x_uid, &x_root, x_who, 'n');
+         getcwd (x_home, LEN_HUND);
+         rc = strlproj (x_home, x_proj);
+         f  = fopen ("/tmp/unit_whoami.txt", "wt");
+         if (f != NULL) {
+            fprintf (f, "pid  : %d\n" , x_pid);
+            fprintf (f, "ppid : %d\n" , x_ppid);
+            fprintf (f, "uid  : %d\n" , x_uid);
+            fprintf (f, "root : %c\n" , x_root);
+            fprintf (f, "who  : %s\n" , x_who);
+            fprintf (f, "home : %s\n" , x_home);
+            fprintf (f, "name : %s\n" , x_proj);
+            fflush  (f);
+            fclose  (f);
+         }
+         rc = chmod ("/tmp/unit_whoami.txt", 0666);
+         return 0;
+      }
+   }
+   /*---(daemonize)----------------------*/
+   if (x_daemon == 'y')  yEXEC_daemon (-1, (int *) 0);
+   yEXEC_signal (x_bullet, x_inter, x_child, incomming, "unit");
+   /*---(loop)---------------------------*/
+   f = fopen ("/tmp/unit_ticker.txt", "wt");
+   i = 0;
+   if (x_cpu == '-') {
+      while (i++ < x_loop) {
+         if (f != NULL) {
+            fprintf (f, ".");
+            fflush  (f);
+         }
+         sleep (1);
+         switch (x_mem) {
+         case 'L' :
+            for (a = 0; a <  10 ; ++a)  b *= a;
+            break;
+         case 'M' :
+            for (a = 0; a < 100 ; ++a)  malloc (1024);
+            break;
+         case 'H' :
+            for (a = 0; a < 200 ; ++a)  b *= a;
+            break;
+         }
+      }
+   } else {
+      alarm (x_loop);
+      while (1) {
+         switch (x_cpu) {
+         case 'L' :
+            ++a;
+            usleep (100);
+            break;
+         case 'M' :
+            for (a = 0; a < 150 ; ++a)  b *= a;
+            usleep (20);
+            break;
+         case 'H' :
+            for (a = 0; a < 3000; ++a)  b *= a;
+            usleep (2);
+            break;
+         }
+      }
+   }
+   if (f != NULL)  fclose (f);
+   chmod ("/tmp/unit_ticker.txt", 0666);
+   if (x_hup >  1) {
+      /*> printf ("hupping %d\n", x_hup);                                             <*/
+      kill (x_hup, 1);
+   }
+   /*---(complete)-----------------------*/
+   return rc;
+}
+
+
---
+++ unit_daemon.h
@@ -0,0 +1,56 @@
+/*===[[ HEADER ]]=============================================================*/
+/*345678901-12345678901-123456789-123456789-123456789-123456789-123456789-123456789-123456789-*/
+
+#define     P_NAME      "unit_daemon"
+#define     P_FOCUS     "PS (programming support)"
+#define     P_NICHE     "ex (process execution)"
+#define     P_SUBJECT   "unit testing process/daemon"
+#define     P_PURPOSE   "simulate process or daemon"
+
+#define     P_NAMESAKE  "agathos daimon (nobel spirit)"
+#define     P_PRONOUNCE "aa·gaa·thohs day·muhn"
+#define     P_HERITAGE  "companion spirit who ensured good luck, health, and protection"
+#define     P_BRIEFLY   "good luck and protection"
+#define     P_IMAGERY   "young man bearing a cornucopia in one hand and poppy in the other"
+#define     P_REASON    "agathos ensures good luck by allowing the testing of processes"
+
+#define     P_ONELINE   P_NAMESAKE " " P_SUBJECT
+
+#define     P_BASENAME  ""
+#define     P_FULLPATH  ""
+#define     P_SUFFIX    ""
+#define     P_CONTENT   ""
+
+#define     P_SYSTEM    "gnu/linux   (powerful, ubiquitous, technical, and hackable)"
+#define     P_LANGUAGE  "ansi-c      (wicked, limitless, universal, and everlasting)"
+#define     P_COMPILER  "gcc 5.3.0"
+#define     P_CODESIZE  "small       (appoximately 1,000 slocl)"
+#define     P_DEPENDS   "ySTR"
+
+#define     P_AUTHOR    "heatherlyrobert"
+#define     P_CREATED   "2015-01"
+
+#define     P_VERMAJOR  "1.--, in production"
+#define     P_VERMINOR  "1.5-, used across several unit tests"
+#define     P_VERNUM    "1.5a"
+#define     P_VERTXT    "created a quiet heartbeat version for signals"
+
+#define     P_PRIORITY  "direct, simple, brief, vigorous, and lucid (h.w. fowler)"
+#define     P_PRINCIPAL "[grow a set] and build your wings on the way down (r. bradbury)"
+#define     P_REMINDER  "there are many better options, but i *own* every byte of this one"
+
+/*345678901-12345678901-123456789-123456789-123456789-123456789-123456789-123456789-123456789-*/
+
+
+
+
+
+#include  <stdio.h>
+#include  <string.h>
+#include  <time.h>
+#include  <unistd.h>
+#include  <signal.h>
+#include  <malloc.h>
+
+#include  <yEXEC.h>
+#include  <ySTR.h>
